f2ee6f41f0f91a081285bc3fcd4cd6dff301e9d9
Futa Ogawa <ogawa0071@gmail.com>
2022-05-03 17:28:21 -0700

    Add Prisma Schema.

---
+++ pages/api/posts.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Post } from "@prisma/client";
+import prisma from "../../lib/prisma";
+
+export default async function assetHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<Post[] | { error: string }>
+) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const posts = await prisma.post.findMany();
+        res.status(200).json(posts);
+      } catch (e) {
+        console.error("Request error", e);
+        res.status(500).json({ error: "Error fetching posts" });
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
