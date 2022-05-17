a513fb092eabde1c5a5862af5bd1288dcecfc6e2
Futa Ogawa <ogawa0071@gmail.com>
2022-05-17 16:09:10 -0700

    Test update.

--- sitemap.ts
+++ sitemap.ts
@@ -2,22 +2,37 @@ import * as fs from 'fs'
 import { prisma } from './lib/prisma'
 import { dateToUrl } from './lib/date'
 import type { Post } from '@prisma/client'
+import * as path from 'path'
 
 const host = 'https://techcrunchjapan.com'
 
 ;(async () => {
-  let posts: { slug: string; createdAt: Date; updatedAt: Date }[] = []
+  let posts: {
+    [key: string]: { slug: string; createdAt: Date; updatedAt: Date }[]
+  } = {}
 
-  for (
-    let index = 0;
-    index < Math.ceil((await prisma.post.count()) / 10000);
-    index++
-  ) {
+  // for (
+  //   let index = 0;
+  //   index < Math.ceil((await prisma.post.count()) / 10000);
+  //   index++
+  // ) {
+
+  const years = new Array(2022 - 2006 + 1)
+    .fill('')
+    .map((_, index) => 2006 + index)
+
+  for (const year of years) {
     const _posts = await prisma.post.findMany({
       select: {
         slug: true,
         createdAt: true,
         updatedAt: true,
+      },
+      where: {
+        createdAt: {
+          gte: `${year}-01-01T00:00:00+09:00`,
+          lt: `${year + 1}-01-01T00:00:00+09:00`,
+        },
       },
       orderBy: {
         createdAt: 'desc',
@@ -25,20 +40,32 @@ const host = 'https://techcrunchjapan.com'
       take: 10000,
     })
 
-    posts = [...posts, ..._posts]
+    posts = { ...posts, [year]: _posts }
   }
 
-  const postsXml = posts.map(
-    (post) =>
-      `<url><loc>${host}${dateToUrl(post.createdAt)}${
-        post.slug
-      }/</loc><lastmod>${post.updatedAt.toISOString()}</lastmod></url>`
-  )
+  // }
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  for (const [year, _posts] of Object.entries(posts)) {
+    const postsXml = _posts.map(
+      (post) =>
+        `<url><loc>${host}${dateToUrl(post.createdAt)}/${post.slug
+          .replaceAll('&', '&amp;')
+          .replaceAll(`'`, '&apos;')
+          .replaceAll(`"`, '&quot;')
+          .replaceAll('>', '&gt;')
+          .replaceAll(
+            '<',
+            '&lt;'
+          )}/</loc><lastmod>${post.updatedAt.toISOString()}</lastmod></url>`
+    )
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${postsXml.join('\n')}
 </urlset>`
 
-  await fs.promises.writeFile('public/sitemap.xml', xml)
+    const _path = `public/${year}/sitemap.xml`
+    await fs.promises.mkdir(path.dirname(_path), { recursive: true })
+    await fs.promises.writeFile(_path, xml)
+  }
 })()
