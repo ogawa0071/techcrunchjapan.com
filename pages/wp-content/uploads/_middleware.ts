f2b9ce814c76d434529f92969d8addbc84bde142
Futa Ogawa <ogawa0071@gmail.com>
2023-05-12 22:25:01 +0900

    GCS to R2.

--- pages/wp-content/uploads/_middleware.ts
+++ pages/wp-content/uploads/_middleware.ts
@@ -2,10 +2,10 @@ import { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl
-  url.hostname = 'storage.googleapis.com'
+  url.hostname = 'r2.techcrunchjapan.com'
   url.port = '443'
   url.protocol = 'https:'
-  url.pathname = `/techcrunchjapan-com${req.nextUrl.pathname}`
+  url.pathname = `/${req.nextUrl.pathname}`
 
   return await fetch(url.toString())
 }
