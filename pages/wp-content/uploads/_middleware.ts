f8ce39f7fee7dff784c77b3a3db62614768198db
Futa Ogawa <ogawa0071@gmail.com>
2023-05-12 22:31:07 +0900

    GCS to R2.

--- pages/wp-content/uploads/_middleware.ts
+++ pages/wp-content/uploads/_middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(req: NextRequest) {
   url.hostname = 'r2.techcrunchjapan.com'
   url.port = '443'
   url.protocol = 'https:'
-  url.pathname = `/${req.nextUrl.pathname}`
+  url.pathname = `${req.nextUrl.pathname.split('/wp-content/uploads')[1]}`
 
   return await fetch(url.toString())
 }
