8914d3d8826893541e2477436a9dff1737a81942
Futa Ogawa <ogawa0071@gmail.com>
2024-12-02 22:01:43 +0900

    Add ads

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -46,6 +46,22 @@ const Sidebar: NextPage = () => {
           </div>
         </div>
       </div>
+      <div>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8137402229295875"
+          crossOrigin="anonymous"
+        ></script>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-8137402229295875"
+          data-ad-slot="2189839244"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      </div>
     </div>
   )
 }
